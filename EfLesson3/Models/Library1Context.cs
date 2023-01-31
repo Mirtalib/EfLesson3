1d0d54d6439db919df3be9b5f59ffa44ecf6ab90
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2023-01-31 21:32:32 +0400

    last Commit

--- EfLesson3/Models/Library1Context.cs
+++ EfLesson3/Models/Library1Context.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
 
 namespace EfLesson3.Models
 {
@@ -31,13 +32,14 @@ namespace EfLesson3.Models
         public virtual DbSet<TestTransaction> TestTransactions { get; set; } = null!;
         public virtual DbSet<Theme> Themes { get; set; } = null!;
 
+
+        string str = "Data Source=.;Initial Catalog=Library1;Integrated Security=True;";
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Library1;Integrated Security=True;");
-            }
+            optionsBuilder
+                .LogTo(Console.WriteLine,LogLevel.Information)
+                .UseSqlServer(str);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
